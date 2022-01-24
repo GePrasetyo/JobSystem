4f85d79d84a919d8bdeee035fcbf2300eb0617c9
Dhimas <dhimas@rocknanoglobal.com>
2022-01-24 22:17:54 +0700

    Fixed PositionUpdateJob.cs

--- JobSystem/Assets/Scripts/ManualThreading/PositionUpdateJob.cs
+++ JobSystem/Assets/Scripts/ManualThreading/PositionUpdateJob.cs
@@ -22,41 +22,41 @@ public class PositionUpdateJob : JobThread
 
     protected override void ThreadFunction()
     {        
-        for (int i = 0; i < ObjectVelocities.Count; i++)
-        {
-            Vector3 currentVelocity = ObjectVelocities[i];
+        //for (int i = 0; i < ObjectVelocities.Count; i++)
+        //{
+        //    Vector3 currentVelocity = ObjectVelocities[i];
 
-            transform.position += transform.localToWorldMatrix.MultiplyVector(new Vector3(0, 0, 1)) * moveSpeed * jobDeltaTime * Random.Range(0.3f, 1.0f);
+        //    transform.position += transform.localToWorldMatrix.MultiplyVector(new Vector3(0, 0, 1)) * moveSpeed * jobDeltaTime * Random.Range(0.3f, 1.0f);
 
-            if (currentVelocity != Vector3.zero)
-            {
-                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(currentVelocity), turnSpeed * jobDeltaTime);
-            }
+        //    if (currentVelocity != Vector3.zero)
+        //    {
+        //        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(currentVelocity), turnSpeed * jobDeltaTime);
+        //    }
 
-            Vector3 currentPosition = transform.position;
+        //    Vector3 currentPosition = transform.position;
 
-            var randomise = true;
+        //    var randomise = true;
 
-            if (currentPosition.x > Center.x + Bounds.x / 2 || currentPosition.x < Center.x - Bounds.x / 2 || currentPosition.z > Center.z + Bounds.y / 2 || currentPosition.z < Center.z - Bounds.y / 2)
-            {
-                Vector3 internalPosition = new Vector3(Center.x + random.Range(-Bounds.x / 2, Bounds.x / 2) / 1.3f, 0, Center.z + Random.Range(-Bounds.y / 2, Bounds.y / 2) / 1.3f);
+        //    if (currentPosition.x > Center.x + Bounds.x / 2 || currentPosition.x < Center.x - Bounds.x / 2 || currentPosition.z > Center.z + Bounds.y / 2 || currentPosition.z < Center.z - Bounds.y / 2)
+        //    {
+        //        Vector3 internalPosition = new Vector3(Center.x + random.Range(-Bounds.x / 2, Bounds.x / 2) / 1.3f, 0, Center.z + Random.Range(-Bounds.y / 2, Bounds.y / 2) / 1.3f);
 
-                currentVelocity = (internalPosition - currentPosition).normalized;
-                ObjectVelocities[i] = currentVelocity;
+        //        currentVelocity = (internalPosition - currentPosition).normalized;
+        //        ObjectVelocities[i] = currentVelocity;
 
-                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(currentVelocity), TurnSpeed * JobDeltaTime * 2);
+        //        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(currentVelocity), TurnSpeed * JobDeltaTime * 2);
 
-                randomise = false;
-            }
+        //        randomise = false;
+        //    }
 
-            if (randomise)
-            {
-                if (Random.Range(0, MoveChangeFrequency) <= 2)
-                {
-                    ObjectVelocities[i] = new Vector3(System.Random Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
-                }
-            }
-        }
+        //    if (randomise)
+        //    {
+        //        if (Random.Range(0, MoveChangeFrequency) <= 2)
+        //        {
+        //            ObjectVelocities[i] = new Vector3(System.Random Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
+        //        }
+        //    }
+        //}
     }
     protected override void OnFinished()
     {
